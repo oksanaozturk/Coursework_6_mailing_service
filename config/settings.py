807f1bb148fb37c7e3af9f002f5e1aadcdf6d4a8
Oksana Ozturk <oksanaozturk@gmail.com>
2024-04-12 17:58:26 +0300

    Добавлен AUTH_USER_MODEL

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
@@ -126,12 +127,24 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+# Отвечает за формирование адреса доступа к статике
+STATIC_URL = "static/"
+
+# Отвечает за место на диске, откуда необходимо подгружать статику
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+# Нужны для настройки/работы с документами, которые загружал пользователь (фото, аватарки, видео, аудиофайлф)
+# /работают также как и STATIC_URL
+# Необходимо создать папку 'media' в корне проекта
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+# Добавляем настройки для работы Приложения User
+AUTH_USER_MODEL = "users.User"
