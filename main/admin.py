516e4904bea0428dc620ac6fb76053f6e7b1af51
casper <oksanaozturk@gmail.com>
2024-04-19 00:37:58 +0300

    Зарегистрированы и настроены для отображения модели Client, Message, Newsletter, Log

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Client, Newsletter, Message, Log
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    """Класс для регистрации модели Клиента в админке."""
+
+    list_display = ("name", "email", "owner")
+    list_filter = ("name",)
+    search_fields = ("email",)
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    """Класс для регистрации модели Рассылки в админке."""
+
+    list_display = ("title", "author", "date_start",
+                    "date_finish", "periodicity", "status", "is_active")
+    list_filter = ("title",)
+    search_fields = ("title", "author")
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    """Класс для регистрации модели Сообщения в админке."""
+
+    list_display = ("subject", "body", "author")
+    search_fields = ("subject", "author")
+
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    """Класс для регистрации модели Лога в админке."""
+
+    list_display = ("newsletter", "last_time_send", "status", "server_response")
+    search_fields = ("newsletter",)
