b6310e1a6bd644b1eb36d2b07993eb2244b3f1e0
casper <oksanaozturk@gmail.com>
2024-06-12 16:01:56 +0300

    Добавлены контроллеры

--- main/views.py
+++ main/views.py
@@ -1,3 +1,132 @@
-from django.shortcuts import render
+from django.forms import inlineformset_factory
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from main.forms import NewsletterForm, MessageForm, ClientForm
+from main.models import Newsletter, Message, Client
+
+
+class NewsletterListView(ListView):
+    """Класс для отображения всех рассылок"""
+
+    model = Newsletter
+
+
+class NewsletterDetailView(DetailView):
+    """Класс для вывода страницы с одной рассылкой по pk"""
+
+    model = Newsletter
+
+
+class NewsletterCreateView(CreateView):
+    """Класс для создания новой рассылки"""
+
+    model = Newsletter
+    form_class = NewsletterForm
+
+    success_url = reverse_lazy("main:newsletter_list")
+
+    # def form_valid(self, form):
+    #     """Метод для автоматического привязывания Пользователя к создаваемой Рассылке"""
+    #     # Сохранение формы
+    #     self.object = form.save()
+    #     self.object.author = self.request.user
+    #     self.object.save()
+    #
+    #     return super().form_valid(form)
+
+
+class NewsletterUpdateView(UpdateView):
+    """Класс для редактирования рассылки"""
+
+    model = Newsletter
+    form_class = NewsletterForm
+
+    def get_success_url(self):
+        """Метод для определения пути, куда будет совершен переход после редактирования рассылки"""
+        return reverse("main:newsletter_detail", args=[self.get_object().pk])
+
+
+class NewsletterDeleteView(DeleteView):
+    """Класс для удаления рассылки"""
+
+    model = Newsletter
+    success_url = reverse_lazy("main:newsletter_list")
+
+
+class MessageListView(ListView):
+    """Класс для отображения всех созданных сообщений"""
+
+    model = Message
+
+
+class MessageDetailView(DetailView):
+    """Класс для вывода страницы с одним сообщением по pk"""
+
+    model = Message
+
+
+class MessageCreateView(CreateView):
+    """Класс для создания нового сообщения"""
+
+    model = Message
+    form_class = MessageForm
+
+    success_url = reverse_lazy("main:message_list")
+
+
+class MessageUpdateView(UpdateView):
+    """Класс для редактирования сообщения"""
+
+    model = Message
+    form_class = MessageForm
+
+    def get_success_url(self):
+        """Метод для определения пути, куда будет совершен переход после редактирования сообщения"""
+        return reverse("main:message_detail", args=[self.get_object().pk])
+
+
+class MessageDeleteView(DeleteView):
+    """Класс для удаления сообщения"""
+
+    model = Message
+    success_url = reverse_lazy("main:message_list")
+
+
+class ClientListView(ListView):
+    """Класс для отображения всех созданных Клиентов"""
+
+    model = Client
+
+
+class ClientDetailView(DetailView):
+    """Класс для вывода страницы с одним Клиентом по pk"""
+
+    model = Client
+
+
+class ClientCreateView(CreateView):
+    """Класс для создания нового Клиент"""
+
+    model = Client
+    form_class = ClientForm
+
+    success_url = reverse_lazy("main:client_list")
+
+
+class ClientUpdateView(UpdateView):
+    """Класс для редактирования Клиента"""
+
+    model = Client
+    form_class = ClientForm
+
+    def get_success_url(self):
+        """Метод для определения пути, куда будет совершен переход после редактирования сообщения"""
+        return reverse("main:client_detail", args=[self.get_object().pk])
+
+
+class ClientDeleteView(DeleteView):
+    """Класс для удаления Клиента"""
+
+    model = Client
+    success_url = reverse_lazy("main:client_list")
