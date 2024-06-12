b19125a2fc85ddc5c7fe6f4c31853cb646fad3b2
casper <oksanaozturk@gmail.com>
2024-06-12 16:06:26 +0300

    Корректировки Админки

--- main/admin.py
+++ main/admin.py
@@ -26,7 +26,7 @@ class NewsletterAdmin(admin.ModelAdmin):
 class MessageAdmin(admin.ModelAdmin):
     """Класс для регистрации модели Сообщения в админке."""
 
-    list_display = ("subject", "body", "author")
+    list_display = ("id", "subject", "body", "author")
     search_fields = ("subject", "author")
 
 
