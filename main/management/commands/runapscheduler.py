fe7d2b2c7c3437462a826325ab7456493819e228
casper <oksanaozturk@gmail.com>
2024-06-14 08:51:19 +0300

    Настройка работы apscheduler

--- main/management/commands/runapscheduler.py
+++ main/management/commands/runapscheduler.py
@@ -5,41 +5,37 @@ from django.conf import settings
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from django.core.management.base import BaseCommand
-from django.utils import timezone
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 from django_apscheduler import util
 
-from main.models import Newsletter
+from main.services import send_mail_by_time
 
 logger = logging.getLogger(__name__)
 
 
 def my_job():
-    time_now = timezone.now().date()
-    print(time_now)
-    newsletters = Newsletter.objects.filter(date_send=time_now)
-    mailings = Newsletter.objects.filter(time_start__gt=time_now).filter(time_finish_lte=['created', 'start'])
-
-    # send_mail("Верификация почты", message, settings.EMAIL_HOST_USER, [user.email])
-
-
-def my_job1():
-    print("Go")
+    """
+    Функция отправки почты согласно интервалу указанному в подписке на рассылку.
+    """
+    send_mail_by_time()
 
 
 # The `close_old_connections` decorator ensures that database connections, that have become
 # unusable or are obsolete, are closed before and after your job has run. You should use it
 # to wrap any jobs that you schedule that access the Django database in any way.
+# Декоратор close_old_connections гарантирует, что соединения с базой данных, которые стали
+# # непригодны для использования или устарели, закрываются до и после выполнения вашего задания.
+# Вы должны использовать это
+# # для упаковки любых запланированных вами заданий, которые каким-либо образом обращаются к базе данных Django.
 @util.close_old_connections
 def delete_old_job_executions(max_age=604_800):
     """
-    This job deletes APScheduler job execution entries older than `max_age` from the database.
-    It helps to prevent the database from filling up with old historical records that are no
-    longer useful.
+    Это задание удаляет из базы данных записи выполнения заданий APScheduler старше max_age.
+    Это помогает предотвратить заполнение базы данных старыми историческими записями, которые не являются
+    дольше полезно.
 
-    :param max_age: The maximum length of time to retain historical job execution records.
-                    Defaults to 7 days.
+    :param max_age: Максимальный срок хранения исторических записей выполнения заданий. По умолчанию 7 дней.
     """
     DjangoJobExecution.objects.delete_old_job_executions(max_age)
 
@@ -54,17 +50,11 @@ class Command(BaseCommand):
         scheduler.add_job(
             my_job,
             trigger=CronTrigger(second="*/10"),  # Every 10 seconds
-            id="my_job",  # The `id` assigned to each job MUST be unique
+            id="my_job",  # Идентификатор, присвоенный каждому заданию, ДОЛЖЕН быть уникальным.
             max_instances=1,
             replace_existing=True,
         )
-        scheduler.add_job(
-            my_job1,
-            trigger=CronTrigger(second="*/10"),  # Every 10 seconds
-            id="my_job1",  # The `id` assigned to each job MUST be unique
-            max_instances=1,
-            replace_existing=True,
-        )
+
         logger.info("Added job 'my_job'.")
 
         scheduler.add_job(
